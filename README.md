57199d5787b3eed5212c517efbc344a0aaac801c
lytex <julianlpc15@gmail.com>
2024-03-09 13:29:37 +0100

    write to file

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-Genetic programming to find strudel code that succintly expresses a given MIDI pattern
+Genetic programming to find strudel code that succinctly expresses a given MIDI pattern
+
+To install `node_modules`
 
 ```bash
 npm i @strudel/core @strudel/mini --save
+```
+
+```bash
+node test.mjs
 ```
--- test.mjs
+++ test.mjs
@@ -1,13 +1,34 @@
-import * as strudel from "@strudel/core";
 // import mini from "./strudel/packages/mini/mini.mjs";
 import { mini } from "@strudel/mini";
 import { controls } from "@strudel/core";
+import { promises as fs } from "fs";
+
+async function writeToFile(filePath, content) {
+  try {
+    await fs.writeFile(filePath, content);
+    console.log(`File "${filePath}" has been written successfully.`);
+  } catch (error) {
+    console.error(`Error writing to file "${filePath}":`, error);
+  }
+}
 global.note = controls.note;
 global.mini = mini.mini;
 
-// process.stdout.write('Hello world');
-let se = mini("<0 3> 2 5 <3 7>");
-let len = se.queryArc(0, 1).length;
-let a = note(mini("60").fast(len)).add(se);
-console.log(a.queryArc(0, 1));
-// a.pianoroll();
+function evaluate_mini(mini_code, length) {
+  let se = mini(mini_code);
+  let len = se.queryArc(0, 1).length;
+  let a = note(mini("60").fast(len)).add(se);
+  return a.queryArc(0, length).map((x) => `1\t${x.value.note}`);
+  // a.pianoroll();
+}
+
+let mini_code = "<0 3> 2 5 <3 7>";
+writeToFile(
+  "PonyGE2/datasets/Strudel/Train.txt",
+  "len\tnote\n" + evaluate_mini(mini_code, 4).join("\n")
+);
+writeToFile(
+  "PonyGE2/datasets/Strudel/Test.txt",
+  "len\tnote\n" + evaluate_mini(mini_code, 4).join("\n")
+);
+// console.log(evaluate_mini(mini_code, 4).join("\n"));
